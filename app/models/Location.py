2118ab1e0b5e945c063d229388be172b13360dd2
Filza Mazahir <filza.mazahir@gmail.com>
2015-11-21 12:39:28 -0800

    Changed Model file

--- app/models/Location.py
+++ app/models/Location.py
@@ -6,6 +6,9 @@ from system.core.model import Model
 class Location(Model):
     def __init__(self):
         super(Location, self).__init__()
+
+    def get_all_locations(self):
+        print self.db.query_db("SELECT * FROM locations")
     """
 
     def get_all_users(self):
