fe7dda291b4ccafdde822d5272ee6a5cf7143b75
Yanze LIU <yanzeliu.france@gmail.com>
2015-11-22 10:45:21 -0800

    set san jose as defaut display

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -9,8 +9,8 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: {
-      lat: -34.397,
-      lng: 150.644
+      lat: 37.324538,
+      lng: -122.030306
     }
   });
   var geocoder = new google.maps.Geocoder();
@@ -24,13 +24,14 @@ function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
+      $("#error-message").html("");
       resultsMap.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location
       });
     } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+      $("#error-message").html("<p>Address is not availabble.</p>");
     }
   });
 }
