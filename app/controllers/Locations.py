ca5fe429b5cd0d0e706380d3e142bd9900bc2032
Filza Mazahir <filza.mazahir@gmail.com>
2015-11-23 01:04:18 -0800

    select query based on haversine formula of lat and long

--- app/controllers/Locations.py
+++ app/controllers/Locations.py
@@ -9,10 +9,12 @@ class Locations(Controller):
         self.load_model('Location')
 
 
+    #Load the index page - with San Jose as default location
     def index(self):
-        
         return self.load_view('index.html')
 
+
+    #Insert new record into database
     def insert_record(self):
     	data={
         'lat':request.form['lat'],
@@ -22,4 +24,21 @@ class Locations(Controller):
         }
         result=self.models['Location'].insert_into_db(data)
         return redirect('/')
+
+
+    #Get nearby locations from database
+    def get_locations(self):
+        # current_location = {
+        #     'lat':request.form['lat'],
+        #     'lng':request.form['lng']
+        # }
+        
+        #For testing purposes:
+        current_location = {
+            'lat': 37.401714,
+            'lng': -121.955815
+        }
+        nearby_locations = self.models['Location'].get_nearby_locations(current_location)
+        print nearby_locations
+        return redirect ('/')
 
--- app/models/Location.py
+++ app/models/Location.py
@@ -7,12 +7,21 @@ class Location(Model):
     def __init__(self):
         super(Location, self).__init__()
 
-    def get_all_locations(self):
-        print self.db.query_db("SELECT * FROM locations")
-
-        
+    # Insert locations into database
     def insert_into_db(self,data_info):
         query="INSERT INTO locations(lat,lng,comments,address,created_at,updated_at)VALUES(%s,%s,%s,%s,NOW(),NOW())"
         data=[data_info['lat'],data_info['lng'],data_info['comments'],data_info['address']];
         return self.db.query_db(query,data)
+    
+
+    # Get locations within 5 miles, based on the Haversine formula
+    def get_nearby_locations(self, current_location):
+        lat = current_location['lat']
+        lng = current_location['lng']
+        select_query = "SELECT * FROM locations WHERE (2 * 6731*0.621371* ATAN(SQRT(SIN(RADIANS(lat-'%s')/2) * SIN(RADIANS(lat-'%s')/2) + COS(RADIANS('%s')) * COS(RADIANS(lat)) * SIN(RADIANS('%s'-lng)/2) * SIN(RADIANS('%s'-lng)/2)),SQRT(1 - SIN(RADIANS(lat-'%s')/2) * SIN(RADIANS(lat-'%s')/2) + COS(RADIANS('%s')) * COS(RADIANS(lat)) * SIN(RADIANS('%s'-lng)/2) * SIN(RADIANS('%s'-lng)/2)) ) ) < 5"
+        data = [lat,lat,lat,lng,lng,lat,lat,lat,lng,lng]
+        nearby_locations =  self.db.query_db(select_query, data)
+        
+        return nearby_locations
+
     
