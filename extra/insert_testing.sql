0992b3731287011737ecc43c484096f8bf33cb04
Filza Mazahir <filza.mazahir@gmail.com>
2015-11-24 23:56:56 -0800

    added testing data

---
+++ extra/insert_testing.sql
@@ -0,0 +1,45 @@
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.787016, -122.407183, "170 O'Farrell St, San Francisco, CA 94102, USA", "Woman with a baby outside Macy's", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.378037, -122.030366, "121 W Evelyn Ave, Sunnyvale, CA 94086, USA", "A man in need of some clothes by Sunnyvale Caltrain station", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.330980, -121.860186, "777 Story Rd, San Jose, CA 95122, USA", "Outside the Walmart Supercenter, need food and clothes", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.351954, -121.975597, "2734 El Camino Real, Santa Clara, CA 95051, USA", "Girl in need of monetary help outside PAMF", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.422, -122.084, "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA", "Few people looking for food outside Garfield Park in Googleplex", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.4022, -121.956, "1528 Vista Club Cir, Santa Clara, CA 95054, USA", "Someone needs help outside the Bella Vista leasing office", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.418071, -121.956146, "95 Holger Way, San Jose, CA 95134, USA", "A person in need outside Target building", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.341643, -121.890800, "291 N 4th St, San Jose, CA 95112, USA", "Outside San Jose State University, close to La Victoria Taqueria", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.391216, -121.982046, "3000 Mission College Blvd, Santa Clara, CA 95054, USA", "Few people in need outside Santa Clara Community College", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.315037, -121.876801, "111 W Alma Ave, San Jose, CA 95110, USA", "In the DMV plaza", NOW(), NOW());
+
+INSERT INTO locations (lat, lng, address, comment, created_at, updated_at) 
+VALUES 
+(37.319882, -121.948999, "377 Santana Row, San Jose, CA 95128, USA", "Couple of people outside the AMC", NOW(), NOW());
+
+
